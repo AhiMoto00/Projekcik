0dc95a7e8fdb35996a738807f46f17419df6911d
Jakub Komenda <jakub.komenda36@microsoft.wsei.edu.pl>
2022-06-23 14:51:28 +0200

    Dodaj pliki projektów.

---
+++ Projekcik/Model.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+public class WypozyczalniaContext : DbContext
+{
+    public DbSet<Klienci> Klient{ get; set; }
+    public DbSet<Pracownicy> Pracownik { get; set; }
+
+    public string ConnectionString { get; }
+
+    public WypozyczalniaContext(string connectionString)
+    {
+        this.ConnectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(this.ConnectionString);
+    }
+}
+
+public class Klienci
+{
+    public int Id { get; set; }
+    public string Imie { get; set; }
+    public string Nazwisko { get; set; }
+    public string PESEL { get; set; }
+    public string Email { get; set; }
+    public string Telefon { get; set; }
+
+}
+
+public class Pracownicy
+{
+    public int Id { get; set; }
+    public string Imie { get; set; }
+    public string Nazwisko { get; set; }
+    public string Telefon { get; set; }
+
+}
+   
---
+++ Projekcik/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+       
+        string connectionString = @"Data Source=DESKTOP-QR4BK4H;Initial Catalog=Wypozyczalnia;Integrated Security=True";
+
+        using (WypozyczalniaContext db = new WypozyczalniaContext(connectionString))
+        {
+            Console.WriteLine($"Database ConnectionString: {db.ConnectionString}.");
+
+           //Dodawanie
+            Console.WriteLine("Dodaj nowego klienta");
+
+            db.Add(new Klienci { Imie="Kamil",Nazwisko="Krech",PESEL="123456789",Email="lubieplacki@gmail.com", Telefon="213769420" });
+            db.SaveChanges();
+           
+           
+        
+        }
+    }
+}
